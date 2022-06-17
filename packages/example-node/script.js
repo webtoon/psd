86af282efa6b4bef05f8ea6148461230bf01ab28
Yehyoung Kang <yehyoung.kang@webtoonscorp.com>
2022-06-17 16:48:37 +0900

    refactor!: remove named export 'parse()'
    This is an internal method and never meant to be called by users.
    Furthermore, the default export `Psd` class also has a static `parse()`
    method, which could be easily confused with the method.
    Thus, we're removing it from the list of exports.
    
    Also edit Node.js example script, which was previously using this
    `parse()` to print information to the console (indicating successful
    parse). Instead, stringify and print the `Psd` object directly.

--- packages/example-node/script.js
+++ packages/example-node/script.js
@@ -6,7 +6,7 @@ import * as fs from "fs";
 import * as path from "path";
 import {fileURLToPath} from "url";
 
-import * as Psd from "@webtoon/psd";
+import Psd from "@webtoon/psd";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -14,4 +14,4 @@ const file_path = path.resolve(__dirname, "./example.psd");
 const psd_file = fs.readFileSync(file_path);
 
 const psd = Psd.parse(psd_file.buffer);
-console.log(psd.fileHeader);
+console.log(psd);
--- packages/psd/src/index.ts
+++ packages/psd/src/index.ts
@@ -7,6 +7,5 @@ import {Psd} from "./classes";
 export type {Group, Layer, Node, NodeChild, NodeParent, Slice} from "./classes";
 export {ColorMode, Depth, GuideDirection, SliceOrigin} from "./interfaces";
 export type {Guide} from "./interfaces";
-export {parse} from "./methods";
 
 export default Psd;
