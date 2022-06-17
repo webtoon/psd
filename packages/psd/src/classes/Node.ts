c8f1ba0bbfba7f4b67b857d28db92dceff740540
Ye-hyoung Kang <keepyourhonor+github@gmail.com>
2022-06-17 09:04:10 +0900

    feat!: export Group, Layer, Slice as types only
    We only want these to exist as types that users can refer to.
    We don't want users to directly instantiate these classes.
    
    This change breaks the original `traverseNode()` example, which was
    using `instanceof` to narrow a `Node` into `Psd`, `Layer`, `Group`.
    From now on, users now have to check the `type` field to do so.
    To make `traverseNode()` work, we replaced `Node` with a union type of
    `Psd`, `Layer`, and `Group`. We also added `NodeParent` and `NodeChild`
    types to better identify which types can be parents and which can be
    children. The original `Node` interface has been renamed to `NodeBase`.

--- packages/psd/src/classes/Node.ts
+++ packages/psd/src/classes/Node.ts
@@ -2,14 +2,35 @@
 // Copyright 2021-present NAVER WEBTOON
 // MIT License
 
-export interface Node {
-  type: "Group" | "Layer" | "Psd";
-  name: string;
-  parent?: Node;
-  children?: Node[];
-  opacity: number;
-  composedOpacity: number;
-  addChild?: (node: Node) => void;
-  hasChildren?: () => boolean;
-  freeze?: () => void;
+import {PsdError} from "../utils";
+import {Group} from "./Group";
+import {Layer} from "./Layer";
+import {Psd} from "./Psd";
+
+export type Node = Psd | Group | Layer;
+export type NodeParent = Psd | Group;
+export type NodeChild = Group | Layer;
+
+export function isNodeParent(node: Node): node is NodeParent {
+  return node.type === "Psd" || node.type === "Group";
+}
+
+export function isNodeChild(node: Node): node is NodeChild {
+  return node.type === "Group" || node.type === "Layer";
+}
+
+export function assertIsNodeParent(node: Node): asserts node is NodeParent {
+  if (!isNodeParent(node)) {
+    throw new PsdError(
+      `Node (name = '${node.name}', type: '${node.type}') cannot be a parent node`
+    );
+  }
+}
+
+export function assertIsNodeChild(node: Node): asserts node is NodeChild {
+  if (!isNodeChild(node)) {
+    throw new PsdError(
+      `Node (name = '${node.name}', type: '${node.type}') cannot be a child node`
+    );
+  }
 }
