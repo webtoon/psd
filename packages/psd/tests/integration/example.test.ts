24d5408a403629ae796aa689059e03fcfdd243b7
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-10-18 12:55:09 +0200

    Add tests for extra properties

--- packages/psd/tests/integration/example.test.ts
+++ packages/psd/tests/integration/example.test.ts
@@ -8,6 +8,7 @@ import {beforeAll, describe, expect, it} from "vitest";
 
 import type Psd from "../../src/index";
 import PSD, {ColorMode, GuideDirection, SliceOrigin} from "../../src/index";
+import {DimensionUnit, ResolutionUnit} from "../../src/interfaces";
 
 const FIXTURE_DIR = path.join(__dirname, "fixtures/example");
 
@@ -39,6 +40,16 @@ describe.each([
     expect(psd.channelCount).toBe(3); // RGB image has three channels
     expect(psd.colorMode).toBe(ColorMode.Rgb);
     expect(psd.opacity).toBe(255);
+    expect(psd.globalLightAngle).toBe(90);
+    expect(psd.globalLightAltitude).toBe(30);
+    expect(psd.resolutionInfo).toStrictEqual({
+      heightUnit: DimensionUnit.CM,
+      horizontal: 300,
+      horizontalUnit: ResolutionUnit.PixelsPerInch,
+      vertical: 300,
+      verticalUnit: ResolutionUnit.PixelsPerInch,
+      widthUnit: DimensionUnit.CM,
+    });
   });
 
   it("should parse all layers and layer groups", () => {
