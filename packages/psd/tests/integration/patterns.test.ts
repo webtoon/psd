f853eb718cd860a60e647cfacf87a9c3b9cc1bce
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-12-06 14:19:36 +0100

    Add pattern test

--- packages/psd/tests/integration/patterns.test.ts
+++ packages/psd/tests/integration/patterns.test.ts
@@ -4,6 +4,7 @@
 
 import * as fs from "fs";
 import * as path from "path";
+import * as crypto from "crypto";
 import {beforeAll, describe, expect, it} from "vitest";
 
 import type Psd from "../../src/index";
@@ -20,6 +21,21 @@ describe("patterns parsing", () => {
   });
 
   it("parses pattern data", () => {
-    console.log("___psd", psd.patterns);
+    expect(psd.patterns).toHaveLength(1);
+  });
+
+  it("renders pattern", async () => {
+    const [pattern] = psd.patterns;
+    const pixelData = await psd.decodePattern(pattern);
+    expect(pixelData).toHaveLength(361200);
+
+    const hash = crypto.createHash("sha256").update(pixelData).digest("hex");
+
+    // NOTE: when changing the hash, please make sure the result is coherent :)
+    // Either using https://www.npmjs.com/package/canvas or browser build (see README)
+    // This looks like a window with green borders
+    expect(hash).toStrictEqual(
+      "ecc109a64333e792149b04ba74ad9917db3e329228fe3401e338142f872860c8"
+    );
   });
 });
