b936401992c2880a194116302c39b15a521851ad
Yehyoung Kang <keepyourhonor+github@gmail.com>
2023-06-27 19:18:07 +0900

    Remove wee_alloc
    wee_alloc is a security risk as of GHSA-rc23-xxgq-x27g.
    Since we don't use it anyway, let's remove it.

--- packages/decoder/src/lib.rs
+++ packages/decoder/src/lib.rs
@@ -4,12 +4,6 @@
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
